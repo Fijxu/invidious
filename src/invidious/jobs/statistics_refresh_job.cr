9cb75a9efa1c7938284824e0e58cd1824200e78e
Fijxu <fijxu@nadeko.net>
2026-01-16 14:44:07 -0300

    draft

--- src/invidious/jobs/statistics_refresh_job.cr
+++ src/invidious/jobs/statistics_refresh_job.cr
@@ -1,31 +1,47 @@
 class Invidious::Jobs::StatisticsRefreshJob < Invidious::Jobs::BaseJob
-  STATISTICS = {
-    "version"  => "2.0",
-    "software" => {
-      "name"    => "invidious",
-      "version" => "",
-      "branch"  => "",
-    },
-    "openRegistrations" => true,
-    "usage"             => {
-      "users" => {
-        "total"          => 0_i64,
-        "activeHalfyear" => 0_i64,
-        "activeMonth"    => 0_i64,
-      },
-    },
-    "metadata" => {
-      "updatedAt"              => Time.utc.to_unix,
-      "lastChannelRefreshedAt" => 0_i64,
-    },
+  struct Statistics
+    include JSON::Serializable
 
-    #
-    #    "totalRequests" => 0_i64,
-    #    "successfulRequests" => 0_i64
-    #    "ratio"   => 0_i64
-    #
-    "playback" => {} of String => Int64 | Float64,
-  }
+    property version : String = "2.0"
+    property software : Software = Software.from_json("{}")
+
+    struct Software
+      include JSON::Serializable
+
+      property name : String = "invidious"
+      property version : String = ""
+      property branch : String = ""
+    end
+
+    @[JSON::Field(key: "openRegistrations")]
+    property open_registrations : Bool = true
+    property usage : Usage = Usage.from_json("{}")
+
+    struct Usage
+      include JSON::Serializable
+      property users : Users = Users.from_json("{}")
+
+      struct Users
+        include JSON::Serializable
+        property total : Int64 = 0
+        @[JSON::Field(key: "activeHalfYear")]
+        property active_half_year : Int64 = 0
+        @[JSON::Field(key: "activeMonth")]
+        property active_month : Int64 = 0
+      end
+    end
+
+    property metadata : Metadata = Metadata.from_json("{}")
+
+    struct Metadata
+      @[JSON::Field(key: "updatedAt")]
+      property updated_at : Int64 = Time.utc.to_unix
+      @[JSON::Field(key: "lastChannelRefreshedAt")]
+      property last_channel_refreshed_at : Int64 = 0
+    end
+
+    property playback : Hash(String, Int64 | Float64) = {} of String => Int64 | Float64
+  end
 
   private getter db : DB::Database
 
